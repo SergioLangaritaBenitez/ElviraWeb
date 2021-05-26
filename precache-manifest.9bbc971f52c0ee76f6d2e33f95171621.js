e6cd00d0f4acb637c243d614ac3cc559f642ac76
SErgio <sergiolangaritabenitez@gmail.com>
2021-05-26 12:49:13 +0200

    Updates

---
+++ precache-manifest.9bbc971f52c0ee76f6d2e33f95171621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929750924b6336a7a5d8bc5ebd96f273",
+    "url": "/ElviraWeb/index.html"
+  },
+  {
+    "revision": "61e2a5dbd386c970bff2",
+    "url": "/ElviraWeb/static/css/main.1d4dbb04.chunk.css"
+  },
+  {
+    "revision": "b6c9453dff660faf0007",
+    "url": "/ElviraWeb/static/js/2.8d7fa112.chunk.js"
+  },
+  {
+    "revision": "bd334d0ef4906c43eb2db7cca8066d8c",
+    "url": "/ElviraWeb/static/js/2.8d7fa112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e2a5dbd386c970bff2",
+    "url": "/ElviraWeb/static/js/main.70b851d0.chunk.js"
+  },
+  {
+    "revision": "9419e7dc7b8b4a78b52e",
+    "url": "/ElviraWeb/static/js/runtime-main.83524ff7.js"
+  },
+  {
+    "revision": "9b449e49e74c46043cb8f121097b87be",
+    "url": "/ElviraWeb/static/media/presentation.9b449e49.jpg"
+  }
+]);
